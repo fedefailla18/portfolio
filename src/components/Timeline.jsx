cbe6d057e2bf94884f2f37b616e1c7215b986e95
Fede Failla <fedefailla18@gmail.com>
2022-11-19 14:43:22 -0300

    3-timeline done

--- src/components/Timeline.jsx
+++ src/components/Timeline.jsx
@@ -1,7 +1,23 @@
 import React from "react";
+import timeline from "../data/timeline";
+import TimelineItem from "./TimelineItem";
 
 const Timeline = () => {
-  return <div></div>;
+  return (
+    <div className="flex flex-col md:flex-row justify-center my-20">
+      <div className="w-full md:w-7/12">
+        <h2> Timeline </h2>
+        {timeline.map((item) => (
+          <TimelineItem
+            year={item.year}
+            duration={item.duration}
+            title={item.title}
+            details={item.details}
+          ></TimelineItem>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default Timeline;
---
+++ src/data/timeline.js
@@ -0,0 +1,14 @@
+export default [
+  {
+    year: "Today",
+    title: "content creator",
+    duration: "10 years",
+    details: `this is what Ive been working in this project`,
+  },
+  {
+    year: "Today",
+    title: "Software Engineer",
+    duration: "10 years",
+    details: `this is what Ive been working along my entire life`,
+  },
+];
