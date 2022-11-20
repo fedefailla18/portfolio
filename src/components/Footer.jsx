eaf1f28487601b8238ffbb7322d80b56c5eebbcc
Fede Failla <fedefailla18@gmail.com>
2022-11-20 12:22:16 -0300

    4 - footer done

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,14 @@
 import React from "react";
 
 const Footer = () => {
-  return <div></div>;
+  return (
+    <div className="py-5 text-center">
+      <p className="text-sm mt-2 opacity-50">
+        &copy; {new Date().getFullYear()}
+        Federico Failla. All rights reserved.
+      </p>
+    </div>
+  );
 };
 
 export default Footer;
