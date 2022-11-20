e5a747fc407bb1d57695f62ed0493f1b94518d6d
Fede Failla <fedefailla18@gmail.com>
2022-11-20 12:18:12 -0300

    4- contact done

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,7 +1,43 @@
 import React from "react";
 
 const Contact = () => {
-  return <div></div>;
+  return (
+    <div className="flex flex-col mb-10 mx- auto">
+      <div className="flex justify-center items-center">
+        <form
+          action="https://getform.io/f/d1e01ef1-1c75-482b-bd28-efed517ec1d3"
+          method="POST"
+          className="flex flex-col w-full md:w-7/12"
+        >
+          <input
+            type="text"
+            name="name"
+            placeholder="Name"
+            className="p-2 bg-transparent border-2 rounded-md focus:outline-none"
+          ></input>
+          <input
+            type="email"
+            autocomplete="username"
+            name="email"
+            placeholder="Email"
+            className="p-2 bg-transparent border-2 rounded-md focus:outline-none"
+          ></input>
+          <textarea
+            className="p-2 mb-4 bg-transparent border-2 rounded-md focus:outline-none"
+            name="message"
+            placeholder="Message"
+            rows="10"
+          />
+          <button
+            type="button"
+            className="text-center inline-block px-8 py-3 w-max text-base font-medium rounded-md text-white bg-gradient-to-r from-yellow-500 to-pink-500 drop-shadow-md hover:stroke-white"
+          >
+            Work with me
+          </button>
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default Contact;
